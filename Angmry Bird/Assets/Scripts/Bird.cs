d60be8658e680d04fb0fd41cd5ef09c766110ac9
AmalID <amalianeko21@gmail.com>
2021-05-05 06:15:15 +0700

    -

---
+++ Angmry Bird/Assets/Scripts/Bird.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+public enum BirdState { idle,Thrown}
+    public GameObject Parent;
+    public Rigidbody2D Rigidbody;
+    public CircleCollider2D Collider;
+
+    private BirdState _state;
+    private float _minVelocity = 0.0f;
+    private bool _flagDestroy = false;
+
+    void Start()
+    {
+        Rigidbody.bodyType = RigidbodyType2D.Kinematic;
+        Collider.enabled = false;
+        _state = BirdState.idle;
+    }
+
+    void FixedUpdate()
+    {
+        if (_state == BirdState.idle && Rigidbody.velocity.sqrMagnitude < _minVelocity && !_flagDestroy)
+        {
+            _flagDestroy = true;
+            StartCoroutine(DestroyAfter(2));
+        }
+    }
+
+    private IEnumerator DestroyAfter(float second)
+    {
+        yield return new WaitForSeconds(second);
+        Destroy(gameObject);
+    }
+
+    public void MoveTo(Vector2 target, GameObject parent)
+    {
+        gameObject.transform.SetParent(parent.transform);
+        gameObject.transform.position = target;
+    }
+
+    public void Shoot(Vector2 velocity, float distance, float speed)
+    {
+        Collider.enabled = true;
+        Rigidbody.bodyType = RigidbodyType2D.Dynamic;
+        Rigidbody.velocity = velocity * speed * distance;
+    }
+}
--- Angmry Bird/Assets/Scripts/SlingShooter.cs
+++ Angmry Bird/Assets/Scripts/SlingShooter.cs
@@ -6,6 +6,7 @@ public class SlingShooter : MonoBehaviour
 {
     public CircleCollider2D Collider;
     private Vector2 _startPos;
+    private Bird _bird;
 
     [SerializeField] private float _radius = 0.75f;
     [SerializeField] private float _throwSpeed = 30;
@@ -21,8 +22,9 @@ public class SlingShooter : MonoBehaviour
         Vector2 velocity = _startPos - (Vector2)transform.position;
         float distance = Vector2.Distance(_startPos, transform.position);
 
-        gameObject.transform.position = _startPos;
-    }
+        _bird.Shoot(velocity, distance, _throwSpeed);
+
+        gameObject.transform.position = _startPos;    }
 
     void OnMouseDrag()
     {
@@ -34,5 +36,11 @@ public class SlingShooter : MonoBehaviour
             dir = dir.normalized * _radius;
             transform.position = _startPos + dir;
         }
+    }
+    public void InitiateBird(Bird bird)
+    {
+        _bird = bird;
+        _bird.MoveTo(gameObject.transform.position, gameObject);
+        Collider.enabled = true;
     }
 }
